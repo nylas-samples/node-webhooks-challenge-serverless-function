1f793fc0d0d83bc484845fdd22863239536407aa
Ram Bansal <ram.bansal@nylas.com>
2022-07-14 11:21:10 -0400

    cleanup

--- api/webhooks/nylas.js
+++ api/webhooks/nylas.js
@@ -7,14 +7,9 @@ export default function handler(request, response) {
  }
 
  if (request.method === "POST") {
-   console.log('==========BODY DELTAS START==========');
-   console.log(JSON.stringify(request.body));
-  //  if (request.body.deltas[0].metadata) {
-  //      for (key in request.body.deltas[0].metadata) {
-  //          console.log(key + ": " + request.body.deltas[0].metadata[key])
-  //      }
-  //  }
-   console.log('==========BODY DELTAS START==========\n');
+   console.log('==========Message Updated Start==========');
+   request.body.deltas.map(deltas => console.log(JSON.stringify(deltas)));
+   console.log('==========Message Updated End==========\n');
  }
 
   // Responding to Nylas is important to prevent the webhook from retrying
